0df54308637b1b5ce4ea5ef45110a06b97e3d281
Rory Sinclair <rory@mungler.com>
2018-04-25 15:16:05 +0100

    fix incorrect version

--- lib/jedis_rb/vendor.rb
+++ lib/jedis_rb/vendor.rb
@@ -1,3 +1,3 @@
 require 'jar-dependencies'
-require_jar('org.apache.commons', 'commons-pool2', '2.4.1')
+require_jar('org.apache.commons', 'commons-pool2', '2.4.2')
 require_jar('redis.clients', 'jedis', JedisRb::VERSION)
